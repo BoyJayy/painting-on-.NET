d514a41e2b7f0b9f41ddb52fbf7ea8167f638c08
artem popkov <popkov.ad@icloud.com>
2024-03-10 16:09:16 +0300

    Добавьте файлы проекта.

---
+++ drawing1/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace drawing1
+{
+    partial class paint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sliderWidth = new System.Windows.Forms.TrackBar();
+            this.drawPlace = new System.Windows.Forms.PictureBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.colorPickerButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.drawPlace)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sliderWidth
+            // 
+            this.sliderWidth.BackColor = System.Drawing.Color.Black;
+            this.sliderWidth.Location = new System.Drawing.Point(3, 28);
+            this.sliderWidth.Name = "sliderWidth";
+            this.sliderWidth.Size = new System.Drawing.Size(174, 45);
+            this.sliderWidth.TabIndex = 0;
+            this.sliderWidth.Scroll += new System.EventHandler(this.sliderWidth_Scroll);
+            // 
+            // drawPlace
+            // 
+            this.drawPlace.Location = new System.Drawing.Point(-1, -2);
+            this.drawPlace.Name = "drawPlace";
+            this.drawPlace.Size = new System.Drawing.Size(1042, 685);
+            this.drawPlace.TabIndex = 1;
+            this.drawPlace.TabStop = false;
+            this.drawPlace.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawPlace_MouseDown);
+            this.drawPlace.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawPlace_MouseMove);
+            this.drawPlace.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawPlace_MouseUp);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1047, 575);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(209, 51);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(1047, 632);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(209, 51);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Controls.Add(this.button7);
+            this.flowLayoutPanel1.Controls.Add(this.button8);
+            this.flowLayoutPanel1.Controls.Add(this.colorPickerButton);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1059, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(182, 104);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(39, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(75, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Magenta;
+            this.button4.Location = new System.Drawing.Point(111, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(147, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 30);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LimeGreen;
+            this.button6.Location = new System.Drawing.Point(3, 39);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 30);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button7.Location = new System.Drawing.Point(39, 39);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(30, 30);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Blue;
+            this.button8.Location = new System.Drawing.Point(75, 39);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(30, 30);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // colorPickerButton
+            // 
+            this.colorPickerButton.Location = new System.Drawing.Point(3, 75);
+            this.colorPickerButton.Name = "colorPickerButton";
+            this.colorPickerButton.Size = new System.Drawing.Size(75, 23);
+            this.colorPickerButton.TabIndex = 8;
+            this.colorPickerButton.Text = "picker";
+            this.colorPickerButton.UseVisualStyleBackColor = true;
+            this.colorPickerButton.Click += new System.EventHandler(this.colorPickerButton_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.sliderWidth);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(1059, 116);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(182, 104);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose width";
+            // 
+            // paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 695);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.drawPlace);
+            this.Name = "paint";
+            this.Text = "drawing";
+            ((System.ComponentModel.ISupportInitialize)(this.sliderWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.drawPlace)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar sliderWidth;
+        private System.Windows.Forms.PictureBox drawPlace;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button colorPickerButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ drawing1/Form1.cs
@@ -0,0 +1,103 @@
+﻿// Ignore Spelling: Pointsarr
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace drawing1
+{
+    public partial class paint : Form
+    {
+        public paint()
+        {
+            InitializeComponent();
+            setSize();
+        }
+
+        private class PointsArray
+        {
+            private int index = 0;
+            private Point[] points;
+
+            public PointsArray(int size)
+            {
+                if (size <= 0) { size = 2; }
+                points = new Point[size];
+            }
+
+            public void setPoint(int x, int y)
+            {
+                if (index >= points.Length)
+                {
+                    index = 0;
+                }
+                points[index] = new Point(x, y);
+                index++;
+            }
+
+            public void resetIndex()
+            {
+                index = 0;
+            }
+
+            public int getLastIndex()
+            {
+                return index;
+            }
+
+            public Point[] getPoints()
+            {
+                return points;
+            }
+        }
+
+        private PointsArray arrayPoints = new PointsArray(2);
+        private bool isMouse = false;
+        private Bitmap map = new Bitmap(100, 100);
+        private Pen pen = new Pen(Color.Black, 3f);
+        private Graphics g;
+
+        private void setSize()
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds;
+            map = new Bitmap(rectangle.Width, rectangle.Height);
+            g = Graphics.FromImage(map);
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void sliderWidth_Scroll(object sender, EventArgs e)
+        {
+        }
+
+        private void drawPlace_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouse = true;
+        }
+
+        private void drawPlace_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouse = false;
+            arrayPoints.resetIndex();
+
+        }
+
+        private void drawPlace_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouse) { return; }
+            arrayPoints.setPoint(e.X, e.Y);
+            if (arrayPoints.getLastIndex() >= 2)
+            {
+                g.DrawLines(pen, arrayPoints.getPoints());
+                drawPlace.Image = map;
+                arrayPoints.setPoint(e.X, e.Y);
+            }
+        }
+
+        private void colorPickerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
